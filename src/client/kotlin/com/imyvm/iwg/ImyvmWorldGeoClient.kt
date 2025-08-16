61376f0ef0629c458bfc53f6b28b4b6a3ff602b8
Doohaey Re <doohaey@gmail.com>
2025-08-16 23:58:08 +0800

    Initial commit.

---
+++ src/client/kotlin/com/imyvm/iwg/ImyvmWorldGeoClient.kt
@@ -0,0 +1,9 @@
+package com.imyvm.iwg
+
+import net.fabricmc.api.ClientModInitializer
+
+object ImyvmWorldGeoClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
