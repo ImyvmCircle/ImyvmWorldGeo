4d9da6fb58db1784736f2c84aa1eaa35ae835c7a
Doohaey Re <doohaey@gmail.com>
2025-08-20 17:14:02 +0800

    refactor: delete client branch and modify mixin settings.

--- src/client/kotlin/com/imyvm/iwg/ImyvmWorldGeoClient.kt
+++
@@ -1,9 +0,0 @@
-package com.imyvm.iwg
-
-import net.fabricmc.api.ClientModInitializer
-
-object ImyvmWorldGeoClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
