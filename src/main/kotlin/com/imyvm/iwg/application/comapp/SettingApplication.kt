17f20ef0f0649acd866ec6df575f6bd27f1d1bf8
Doohaey Re <doohaey@gmail.com>
2025-09-24 00:16:51 +0800

    feat: keyType functions.

--- src/main/kotlin/com/imyvm/iwg/application/comapp/SettingApplication.kt
+++ src/main/kotlin/com/imyvm/iwg/application/comapp/SettingApplication.kt
@@ -1,6 +1,9 @@
 package com.imyvm.iwg.application.comapp
 
+import com.imyvm.iwg.domain.EffectKey
+import com.imyvm.iwg.domain.PermissionKey
 import com.imyvm.iwg.domain.Region
+import com.imyvm.iwg.domain.RuleKey
 import net.minecraft.server.network.ServerPlayerEntity
 
 fun onHandleSetting(
@@ -12,48 +15,9 @@ fun onHandleSetting(
     isPersonal: Boolean,
     targetPlayerStr: String
 ){
-    if (scopeName.isNullOrBlank()) {
-        onHandleRegion(
-            player,
-            region,
-            keyString,
-            valueString,
-            isPersonal,
-            targetPlayerStr
-        )
-    } else {
-        onHandleScope(
-            player,
-            region,
-            scopeName,
-            keyString,
-            valueString,
-            isPersonal,
-            targetPlayerStr
-        )
-    }
-}
-
-private fun onHandleRegion(
-    player: ServerPlayerEntity,
-    region: Region,
-    keyString: String,
-    valueString: String,
-    isPersonal: Boolean,
-    targetPlayerStr: String
-){
 
 }
 
-private fun onHandleScope(
-    player: ServerPlayerEntity,
-    region: Region,
-    scopeName: String?,
-    keyString: String,
-    valueString: String,
-    isPersonal: Boolean,
-    targetPlayerStr: String
-) {
-
-}
-
+private fun isPermissionKey(key: String) = runCatching { PermissionKey.valueOf(key) }.isSuccess
+private fun isEffectKey(key: String) = runCatching { EffectKey.valueOf(key) }.isSuccess
+private fun isRuleKey(key: String) = runCatching { RuleKey.valueOf(key) }.isSuccess
