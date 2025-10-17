43c37eb24da331f2bdd5b0e0cb6261708e651c8a
Doohaey Re <doohaey@gmail.com>
2025-10-18 00:21:51 +0800

    feat: RegionDataApi through existing application.

--- src/main/kotlin/com/imyvm/iwg/inter/api/RegionDataApi.kt
+++ src/main/kotlin/com/imyvm/iwg/inter/api/RegionDataApi.kt
@@ -1,5 +1,7 @@
 package com.imyvm.iwg.inter.api
 
+import com.imyvm.iwg.application.region.filterRegionsByMark
+import com.imyvm.iwg.application.region.parseFoundingTimeFromRegionId
 import com.imyvm.iwg.domain.Region
 import com.imyvm.iwg.infra.RegionDatabase
 import net.minecraft.util.math.BlockPos
@@ -9,10 +11,13 @@ object RegionDataApi {
     fun addRegion(region: Region) = RegionDatabase.addRegion(region)
     fun removeRegion(region: Region) = RegionDatabase.removeRegion(region)
     fun renameRegion(region: Region, newName: String) = RegionDatabase.renameRegion(region, newName)
-    fun getRegionList() = RegionDatabase.getRegionList()
-    fun getRegionById(id: Int) = RegionDatabase.getRegionByNumberId(id)
+    fun getRegion(id: Int) = RegionDatabase.getRegionByNumberId(id)
     fun getRegionScopePairByLocation(x: Int, z: Int) = RegionDatabase.getRegionAndScopeAt(x,z)
     fun getRegionScopePairByLocation(blockPos: BlockPos) = RegionDatabase.getRegionAndScopeAt(blockPos.x, blockPos.z)
     fun getRegionArea(region: Region) = region.calculateTotalArea()
-    fun getRegionAreaById(id: Int) = RegionDatabase.getRegionByNumberId(id).calculateTotalArea()
+    fun getRegionArea(id: Int) = RegionDatabase.getRegionByNumberId(id).calculateTotalArea()
+    fun getRegionFoundingTime(region: Region) = parseFoundingTimeFromRegionId(region.numberID)
+    fun getRegionFoundingTime(id: Int) = parseFoundingTimeFromRegionId(id)
+    fun getRegionList() = RegionDatabase.getRegionList()
+    fun getRegionListFiltered(idMark: Int) = filterRegionsByMark(idMark)
 }
