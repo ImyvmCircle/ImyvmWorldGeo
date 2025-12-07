b8c986e3cf9c18a94be5353c8caa287a65311338
Doohaey Re <doohaey@gmail.com>
2025-12-07 13:33:00 +0800

    refactor: merge getPlayerRegionInfo to command argument getter

--- src/main/kotlin/com/imyvm/iwg/inter/register/command/helper/CommandArgumentGetter.kt
+++ src/main/kotlin/com/imyvm/iwg/inter/register/command/helper/CommandArgumentGetter.kt
@@ -1,15 +1,11 @@
 package com.imyvm.iwg.inter.register.command.helper
 
+import com.imyvm.iwg.domain.Region
+import com.imyvm.iwg.infra.RegionDatabase
+import com.imyvm.iwg.util.text.Translator
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.network.ServerPlayerEntity
-
-fun getPlayerRegionPair(context: CommandContext<ServerCommandSource>): Pair<ServerPlayerEntity, String>? {
-    val player = context.source.player ?: return null
-    val regionIdentifier = context.getArgument("regionIdentifier", String::class.java)
-    return player to regionIdentifier
-}
-
 
 fun getOptionalArgument(
     context: CommandContext<ServerCommandSource>,
@@ -37,4 +33,27 @@ fun getPosArgument(
             else -> null
         }
     }
+}
+
+fun getPlayerAndRegionInfo(context: CommandContext<ServerCommandSource>): Triple<ServerPlayerEntity, Region, String>? {
+    val player = context.source.player ?: return null
+    val x = player.blockX
+    val z = player.blockZ
+    val regionScopePair = RegionDatabase.getRegionAndScopeAt(player.world, x, z)
+
+    if (regionScopePair == null) {
+        player.sendMessage(Translator.tr("interaction.meta.scope.teleport_point.no_region"))
+        return null
+    }
+
+    val region = regionScopePair.first
+    val scopeName = regionScopePair.second.scopeName
+
+    return Triple(player, region, scopeName)
+}
+
+fun getPlayerRegionPair(context: CommandContext<ServerCommandSource>): Pair<ServerPlayerEntity, String>? {
+    val player = context.source.player ?: return null
+    val regionIdentifier = context.getArgument("regionIdentifier", String::class.java)
+    return player to regionIdentifier
 }
